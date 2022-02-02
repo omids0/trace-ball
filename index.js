20a3939cd04d09f7f530ded7d1cb8487fe6ab673
omids0 <soheilniao@gmail.com>
2022-02-02 19:17:58 +0330

    pc ball added

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const myballEL = document.getElementById('myball')
+const systemBallEl = document.getElementById('pcBall')
+
 let interval;
 let both = 0
+
+
+let windowSize = window.innerWidth
+let random = Math.floor(Math.random() * windowSize/2)
+
+function setPosition() {
+      let left = parseInt(window.getComputedStyle(systemBallEl).getPropertyValue('left'))
+      systemBallEl.style.left = left + random + 'px'
+
+      
+      let top = parseInt(window.getComputedStyle(systemBallEl).getPropertyValue('top'))
+      systemBallEl.style.top = top + random + 'px'
+
+      console.log(random);
+}
 
 function moveLeft() {
       let left = parseInt(window.getComputedStyle(myballEL).getPropertyValue('left'))
@@ -22,7 +39,10 @@ function moveDown() {
       myballEL.style.top = top + 2 + 'px'
 }
 
+setPosition()
+
 window.addEventListener('keydown', (e) => {
+      
       console.log(e.key);
       if (both == 0) {
             both++;
