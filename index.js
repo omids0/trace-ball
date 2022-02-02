a75ad04796fa706b8991b9bd722c3b07380736d9
omids0 <soheilniao@gmail.com>
2022-02-02 18:20:41 +0330

    my ball added

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log('teste bazi');
+const myballEL = document.getElementById('myball')
+let interval;
+let both = 0
+
+function moveLeft() {
+      let left = parseInt(window.getComputedStyle(myballEL).getPropertyValue('left'))
+      myballEL.style.left = left - 2 + 'px'
+}
+
+function moveRight() {
+      let left = parseInt(window.getComputedStyle(myballEL).getPropertyValue('left'))
+      myballEL.style.left = left + 2 + 'px'
+}
+
+function moveTop() {
+      let top = parseInt(window.getComputedStyle(myballEL).getPropertyValue('top'))
+      myballEL.style.top = top - 2 + 'px'
+}
+
+function moveDown() {
+      let top = parseInt(window.getComputedStyle(myballEL).getPropertyValue('top'))
+      myballEL.style.top = top + 2 + 'px'
+}
+
+window.addEventListener('keydown', (e) => {
+      console.log(e.key);
+      if (both == 0) {
+            both++;
+            if (e.key === 'ArrowLeft') {
+                  interval = setInterval(moveLeft, 1)
+            }
+
+            if (e.key === 'ArrowRight') {
+                  interval = setInterval(moveRight, 1)
+            }
+
+            if (e.key === 'ArrowUp') {
+                  interval = setInterval(moveTop, 1)
+            }
+
+            if (e.key === 'ArrowDown') {
+                  interval = setInterval(moveDown, 1)
+            }
+      }
+})
+
+window.addEventListener('keyup', (e) => {
+      clearInterval(interval)
+      both = 0
+})
