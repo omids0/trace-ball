6e2b6b4caf76482c112a6aeaed407ad61fe14f73
omids0 <soheilniao@gmail.com>
2022-02-02 19:45:41 +0330

    winner function added

--- index.js
+++ index.js
@@ -4,46 +4,60 @@ const systemBallEl = document.getElementById('pcBall')
 let interval;
 let both = 0
 
-
 let windowSize = window.innerWidth
-let random = Math.floor(Math.random() * windowSize/2)
+let random = Math.floor(Math.random() * 500)
 
 function setPosition() {
       let left = parseInt(window.getComputedStyle(systemBallEl).getPropertyValue('left'))
       systemBallEl.style.left = left + random + 'px'
 
-      
+
       let top = parseInt(window.getComputedStyle(systemBallEl).getPropertyValue('top'))
       systemBallEl.style.top = top + random + 'px'
+}
 
-      console.log(random);
+function tracing() {
+      let pcBallLeft = parseInt(window.getComputedStyle(systemBallEl).getPropertyValue('left'))
+      let pcBallTop = parseInt(window.getComputedStyle(systemBallEl).getPropertyValue('top'))
+
+      let left = parseInt(window.getComputedStyle(myballEL).getPropertyValue('left'))
+      let top = parseInt(window.getComputedStyle(myballEL).getPropertyValue('top'))
+      let leftMinus = pcBallLeft - left
+      let topMinus = top - pcBallTop
+
+      if (leftMinus < 50 && topMinus < 50) {
+            window.location.reload()
+            alert('YOU WON!')
+      }
 }
 
 function moveLeft() {
       let left = parseInt(window.getComputedStyle(myballEL).getPropertyValue('left'))
       myballEL.style.left = left - 2 + 'px'
+      tracing()
 }
 
 function moveRight() {
       let left = parseInt(window.getComputedStyle(myballEL).getPropertyValue('left'))
       myballEL.style.left = left + 2 + 'px'
+      tracing()
 }
 
 function moveTop() {
       let top = parseInt(window.getComputedStyle(myballEL).getPropertyValue('top'))
       myballEL.style.top = top - 2 + 'px'
+      tracing()
 }
 
 function moveDown() {
       let top = parseInt(window.getComputedStyle(myballEL).getPropertyValue('top'))
       myballEL.style.top = top + 2 + 'px'
+      tracing()
 }
 
 setPosition()
 
 window.addEventListener('keydown', (e) => {
-      
-      console.log(e.key);
       if (both == 0) {
             both++;
             if (e.key === 'ArrowLeft') {
